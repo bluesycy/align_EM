152b582f4cdb837da6dc4262bfbf02a5cc37826e
Chuyu Yang <bluesycy@gmail.com>
2025-11-20 14:35:32 +0100

    test iterative registration (using pencil kernel), clean up code

--- cmtk_no_init.sh
+++ cmtk_no_init.sh
@@ -4,12 +4,13 @@ IMG_PATH=$1
 REF_PATH=$2
 RESULT_PATH=$3
 XYZ_PATH=$4
+ENERGY_WEIGHT=$5
 
 echo "registration"
 cmtk registration --nmi --verbose-level 6 --dofs 6,9,12 --exploration 8 --accuracy 0.5 -o $RESULT_PATH/affine.list $REF_PATH $IMG_PATH 
 
 echo "warp"
-cmtk warp --nmi --verbose-level 6 --energy-weight 1e-1 --exploration 4 --fast --accuracy 0.5 --grid-spacing 32 --refine 3 --output-intermediate -o $RESULT_PATH/warp.list $REF_PATH $IMG_PATH $RESULT_PATH/affine.list
+cmtk warp --nmi --verbose-level 6 --energy-weight $ENERGY_WEIGHT --exploration 4 --fast --accuracy 0.5 --grid-spacing 32 --refine 3 --output-intermediate -o $RESULT_PATH/warp.list $REF_PATH $IMG_PATH $RESULT_PATH/affine.list
 
 echo "reformatx"
 cmtk reformatx --pad-out 0 -o $RESULT_PATH/affine.nrrd --floating $IMG_PATH $REF_PATH $RESULT_PATH/affine.list
